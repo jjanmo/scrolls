3af4fdf3a55196a2184a76d69cfcce77e073a5f2
jjanmo <jjanmo07@hanmail.net>
2023-08-19 14:24:55 +0900

    feat: configure custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,21 @@
+import { colors } from './src/styles/theme'
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/styles/**/*.{js,ts}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        gray01: colors.gray01,
+        gray02: colors.gray02,
+        gray03: colors.gray03,
+        white: colors.white,
+        green: colors.green,
+        blue: colors.blue,
       },
     },
   },
