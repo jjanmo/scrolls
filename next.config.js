0681147503804679c2832f85348fd91322485354
jjanmo <jjanmo07@hanmail.net>
2023-08-22 03:21:06 +0900

    chores: fix config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false, // 한번 더 리랜더링 되는 부분 때문에 우선 false로 수정함 → 추후 true로 변경
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'jpassets.jobplanet.co.kr',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
