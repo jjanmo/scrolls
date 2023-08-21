96b1e51618b08e7e958b15f8c2a223b5e91bf1bc
jjanmo <jjanmo07@hanmail.net>
2023-08-22 03:19:07 +0900

    feat: how to use hooks about inifinite hook and intersection hook

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,30 +1,52 @@
 import Card from './Card'
 import Spinner from './iocns/Spinner'
-import { useEffect } from 'react'
-import useInfiniteRecruits from '@/hooks/useInfiniteRecruits'
+import { useCallback, useEffect, useState } from 'react'
+import useInfiniteRecruits, { LIMIT_PAGE } from '@/hooks/useInfiniteRecruits'
+import useIntersection from '@/hooks/useIntersection'
 
 export default function List() {
+  const [shouldLoadMore, setShouldLoadMore] = useState<boolean>(false)
   const { currentPage, setPage, isLoading, recruits } = useInfiniteRecruits()
 
-  const handleClick = () => {
-    setPage(currentPage + 1)
-  }
+  const onIntersection = useCallback(
+    (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && currentPage <= LIMIT_PAGE) {
+          setShouldLoadMore(true)
+        } else {
+          setShouldLoadMore(false)
+        }
+      })
+    },
+    [currentPage]
+  )
+  const { target } = useIntersection<HTMLDivElement>({ onIntersection })
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    if (shouldLoadMore) {
+      setPage((prev) => prev + 1)
+    }
+  }, [setPage, shouldLoadMore])
 
   if (isLoading)
     return (
-      <div className="flex w-full items-center justify-center opacity-70">
+      <div className="flex h-[calc(100vh-220px)] w-full items-center justify-center opacity-70">
         <Spinner size={200} />
       </div>
     )
 
   return (
     <>
-      <button onClick={handleClick}>More</button>
       <ul className="mx-auto mt-10 grid w-[900px] grid-cols-3 grid-rows-3 justify-items-center gap-x-20 gap-y-8">
         {recruits?.map((recruit) => <Card key={recruit.id} {...recruit} />)}
       </ul>
+
+      <div ref={target}></div>
+      {shouldLoadMore && (
+        <div className="flex w-full justify-center pt-10">
+          <Spinner size={50} />
+        </div>
+      )}
     </>
   )
 }
