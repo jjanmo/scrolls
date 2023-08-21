e2a520b4c026b9ccb12aad8550c9ca11016c7bc9
jjanmo <jjanmo07@hanmail.net>
2023-08-22 03:18:10 +0900

    feat: add intersection observer hook

---
+++ src/hooks/useIntersection.ts
@@ -0,0 +1,43 @@
+import { useEffect, useRef } from 'react'
+import throttle from 'lodash/throttle'
+
+// eslint-disable-next-line no-unused-vars
+type OnIntersection = (entries: IntersectionObserverEntry[], observer?: IntersectionObserver) => void
+interface Props {
+  onIntersection: OnIntersection
+  options?: {
+    root: Element | Document | null
+    rootMargin: string
+    threshold: number
+  }
+}
+
+const WAIT = 500
+
+export default function useIntersection<T extends Element>({
+  onIntersection,
+  options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+  },
+}: Props) {
+  const target = useRef<T>(null)
+
+  const intersectionCallback: IntersectionObserverCallback = throttle(onIntersection, WAIT)
+
+  useEffect(() => {
+    if (!target.current) return
+
+    const observer = new IntersectionObserver(intersectionCallback, options)
+    observer.observe(target.current as T)
+
+    return () => {
+      observer && observer.disconnect()
+    }
+  }, [intersectionCallback, options])
+
+  return {
+    target,
+  }
+}
