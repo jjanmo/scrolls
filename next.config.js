e70698b654b800cc49d13b64266506f590669020
jjanmo <jjanmo07@hanmail.net>
2024-11-27 01:09:17 +0900

    refactor: 중요 패키지 최신화 및 기술 스택 변경

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, // 한번 더 리랜더링 되는 부분 때문에 우선 false로 수정함 → 추후 true로 변경
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -8,6 +8,9 @@ const nextConfig = {
         hostname: 'jpassets.jobplanet.co.kr',
       },
     ],
+  },
+  compiler: {
+    emotion: true,
   },
 }
 
