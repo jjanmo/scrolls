437fbd5b2141fa446eae51b67dc5b400dd279f06
jjanmo <jjanmo07@hanmail.net>
2024-12-06 22:25:09 +0900

    feat: movie typing 추가

--- src/queries/tmdb.ts
+++ src/queries/tmdb.ts
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 
 import { tmdbApi } from '@/apis'
+import type { MoviesResponse } from '@/types/tmdb'
 
 const getPopularMovies = ({ page = 1 }: { page?: number } = {}) =>
-  tmdbApi.get(`/movie/popular?language=ko-KR&page=${page}`)
+  tmdbApi.get<MoviesResponse>(`/movie/popular?language=ko-KR&page=${page}`)
 
 export const usePopularMoviesQuery = () => {
   return useQuery({
