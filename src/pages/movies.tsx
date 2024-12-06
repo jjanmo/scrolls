fac2f377f1a88aeff58e2641658fa38175dbccae
jjanmo <jjanmo07@hanmail.net>
2024-12-06 22:25:31 +0900

    feat: movie-list 구현

--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -1,12 +1,28 @@
+import styled from '@emotion/styled'
+
+import Movie from '@/components/Movie'
 import { usePopularMoviesQuery } from '@/queries/tmdb'
 
 export default function Movies() {
   const { data, isPending } = usePopularMoviesQuery()
 
-  return (
-    <>
-      <div>This is Movie Page</div>
-      {isPending ? <div>Loading...</div> : JSON.stringify(data)}
-    </>
-  )
+  if (isPending) return <div>Loading...</div>
+
+  return <List>{data?.results.map((movie) => <Movie key={movie.id} {...movie} />)}</List>
 }
+
+const List = styled.ul`
+  width: fit-content;
+  margin: 20px auto;
+  display: grid;
+  grid-template-columns: repeat(4, 140px);
+  gap: 8px;
+
+  @media (max-width: 600px) {
+    grid-template-columns: repeat(2, 140px);
+  }
+
+  @media (max-width: 400px) {
+    grid-template-columns: repeat(1, 140px);
+  }
+`
