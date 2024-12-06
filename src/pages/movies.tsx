15f8d58a1ed25d334e4dd66baeb01514b0c13bcb
jjanmo <jjanmo07@hanmail.net>
2024-12-06 22:58:57 +0900

    feat: infinite-query로 버튼 스타일 리스트 구현

--- src/pages/movies.tsx
+++ src/pages/movies.tsx
@@ -1,14 +1,26 @@
 import styled from '@emotion/styled'
 
 import Movie from '@/components/Movie'
-import { usePopularMoviesQuery } from '@/queries/tmdb'
+import { useInfinitePopularMoviesQuery } from '@/queries/tmdb'
+import { palette } from '@/styles/theme'
 
 export default function Movies() {
-  const { data, isPending } = usePopularMoviesQuery()
+  const { data, isPending, fetchNextPage, hasNextPage } = useInfinitePopularMoviesQuery()
+  const movies = data?.pages.flatMap((page) => page.results)
 
+  const handleMoreClick = () => {
+    if (hasNextPage) fetchNextPage()
+  }
   if (isPending) return <div>Loading...</div>
 
-  return <List>{data?.results.map((movie) => <Movie key={movie.id} {...movie} />)}</List>
+  return (
+    <div>
+      <List>{movies?.map((movie, index) => <Movie key={`${movie.id}-${index}`} {...movie} />)}</List>
+      <MoreButton onClick={handleMoreClick} disabled={!hasNextPage}>
+        {isPending ? 'Loading...' : 'More'}
+      </MoreButton>
+    </div>
+  )
 }
 
 const List = styled.ul`
@@ -24,5 +36,21 @@ const List = styled.ul`
 
   @media (max-width: 400px) {
     grid-template-columns: repeat(1, 140px);
+  }
+`
+
+const MoreButton = styled.button`
+  width: 160px;
+  padding: 4px 0;
+  display: block;
+  margin: 0 auto;
+  background-color: ${palette.blue};
+  color: ${palette.white};
+  border-radius: 4px;
+  border-top: 1px solid transparent;
+
+  &:disabled {
+    background-color: ${palette.gray02};
+    cursor: not-allowed;
   }
 `
--- src/queries/tmdb.ts
+++ src/queries/tmdb.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query'
 
 import { tmdbApi } from '@/apis'
 import type { MoviesResponse } from '@/types/tmdb'
@@ -6,9 +6,18 @@ import type { MoviesResponse } from '@/types/tmdb'
 const getPopularMovies = ({ page = 1 }: { page?: number } = {}) =>
   tmdbApi.get<MoviesResponse>(`/movie/popular?language=ko-KR&page=${page}`)
 
-export const usePopularMoviesQuery = () => {
+export const usePopularMoviesQuery = ({ page = 1 }: { page?: number } = {}) => {
   return useQuery({
+    queryKey: ['popularMovies', page],
+    queryFn: () => getPopularMovies({ page }).then((res) => res.data),
+  })
+}
+
+export const useInfinitePopularMoviesQuery = () => {
+  return useInfiniteQuery({
     queryKey: ['popularMovies'],
-    queryFn: () => getPopularMovies().then((res) => res.data),
+    queryFn: ({ pageParam = 1 }) => getPopularMovies({ page: pageParam }).then((res) => res.data),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.page + 1,
   })
 }
